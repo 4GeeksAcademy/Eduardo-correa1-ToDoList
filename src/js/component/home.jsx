1b64adc3aa8aa62dfa2584a25a9c4ac0eb54b136
Eduardo-correa1 <159133054+Eduardo-correa1@users.noreply.github.com>
2024-05-30 16:08:07 +0000

    tareas

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -5,21 +5,28 @@ import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [inputValue, setInputValue] = useState("");
+	const [task, setTask] = useState([]);
 	return (
 		<div className="text-center container">
 			<h1 className="text-center mt-5">Mis tareas pendientes</h1>
 			<div className="taskContainer">
 			<ul className="mt-5 text-center">
-				<li><input type="text" placeholder="Que tienes por hacer?" /></li>
-				<li><i className="fas fa-trash-alt float-left"></i> Limpiar</li>
-
+				<li><input type="text" placeholder="Agrega tu tarea" onChange={(tarea)=> setInputValue(tarea.target.value)} value={inputValue} onKeyPress={(tarea) =>{
+					if (tarea.key === "Enter"){
+						setTask(task.concat(inputValue));
+						setInputValue("");
+					}
+				}} /></li>
+				{task.map((entrada, index) => <li key={index}><i onClick={()=>setTask(task.filter((entrada, currentIndex) => index != currentIndex))} className="fas fa-trash-alt"></i> {entrada}</li>)}
 			</ul>
-			<div className="text-start taskCounter"><p>10 tareas</p></div>
+			<div className="text-start taskCounter"><p>{task.length}</p></div>
 			</div>
 			
 
 		</div>
 	);
+	
 };
 
 export default Home;
